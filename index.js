17b289f6a374dcc2ce6ba513e42e5e72f61c26ab
Guillermo <p.arcoiris.alberti05@gmail.com>
2026-09-12 13:26:13 -0300

    Forzar IPv4 para envio de email

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const pool = require('./db');
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  family: 4
 });
-
 async function enviarEmailConfirmacion(pedido) {
   try {
     let itemsHtml = '';
